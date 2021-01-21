69a4127d1ecfc4d6769279b8d5b3c9ab84e853d6
avitiello62 <a.vitiello62@stunde.unisa.it>
2021-01-21 14:58:49 +0100

    utils commented

--- pepper_exercise/src/utils/Detector.py
+++ pepper_exercise/src/utils/Detector.py
@@ -1,13 +1,17 @@
-import cv2
 import tensorflow as tf
 assert(int(tf.__version__.split('.')[0]) >= 2)
 import numpy as np
 
 class Detector():
+'''
+Classe Detector che istanzia un detector preaddestrato con la libreria tensorflow
+'''
     def __init__(self,model_path):
+        '''load del modello da file'''
         self.detect_fn = tf.saved_model.load(model_path)
 
     def __call__(self, img, threshold=0.5):
+        '''chiamata al detector che restituisce le detection sull'immagine passata'''
         img = img[:,:,::-1]
         input_tensor = tf.convert_to_tensor(img)
         input_tensor = input_tensor[tf.newaxis, ...]
